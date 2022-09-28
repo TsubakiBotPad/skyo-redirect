9bce98b2965a69e5c1295b01766ab915b80dbf86
TsubakiBot <TsubakiBotPAD@gmail.com>
2022-09-28 00:46:33 +0000

    Change port of server from 80 -> 8000. Also removes 443 port in favor of nginx

--- server.js
+++ server.js
@@ -172,11 +172,12 @@ function findDungeonLink(dgid) {
 }
 
 // Start Server
-http.createServer(app).listen(80, () => console.log(`HTTP listening at port 80`));
-
+http.createServer(app).listen(8000, () => console.log(`HTTP listening at port 80`));
+/*
 https.globalAgent.options.ca = require('ssl-root-cas').create();
 certs = require('./certs.json');
 https.createServer({
   key: fs.readFileSync(certs['key']),
   cert: fs.readFileSync(certs['cert'])
 }, app).listen(443, () => console.log(`HTTPS listening at port 443`));
+*/
